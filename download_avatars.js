0b2be8e3c3c5f666dd7f063d52d866710ce2b8b7
anithaamarnath <anithaamarnath04@gmail.com>
2019-04-11 03:32:40 +0000

    the image download

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,17 @@
 var request = require('request');
+var fs = require('fs');
+var token = require('./secret');
+var args = process.argv;
+
+if(args[2] && args[3]){
+  getRepoContributors(args[2],args[3], function(err,result){
+    console.log("erros", err);
+    printAvatar(result);
+
+
+  })
+}
+
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -7,20 +20,55 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'request'
+      'User-Agent': 'request',
+      'Authorization' : 'Bearer '+token.GITHUB_TOKEN,
     }
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    var contributors = JSON.parse(body);
+    cb(err, contributors);
   });
 }
-function downloadImageByURL(url, filePath) {
-  // ...
+function printAvatar(contributors){
+  for(var i = 0 ; i < contributors.length; i++){
+    //console.log("This is a contributors:" ,contributors);
+    var contributor = contributors[i];
+    var login = contributor.login
+    var avatarUrl = contributor.avatar_url;
+    //console.log("this is obj",avatarUrl);
+    var filePath = './avatar/' + login+".jpg";
+    console.log('filePath ', filePath)
+    //console.log('filePath', filePath)
+    //console.log(filePath);
+    var  url = avatarUrl;
+
+    //console.log("test ",url, avatarUrl);
+    //console.log(url);
+    downloadImageByURL(url, filePath);
+
+  }
+
 }
 
+ function downloadImageByURL(url, filePath) {
+    request.get('https://api.github.com/repos/')
+       .on('error', function (err) {
+        throw err;
+       })
 
+       .pipe(fs.createWriteStream(filePath));
+
+
+}
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+
+
+  // console.log("Errors:", err);
+  // console.log("Result:", result);
+  printAvatar(result);
 });
+//getRepoContributors
+//printAvatar(contributors)
+
+
